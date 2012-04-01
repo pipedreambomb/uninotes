2e0a458e0f8c033889ae716c1457a6f48c2cacdd
George Nixon <g@georgenixon.co.uk>
2012-04-01 13:38:03 +0100

    Name not optional for org, subj

--- app/models/organization.php
+++ app/models/organization.php
@@ -2,6 +2,18 @@
 class Organization extends AppModel {
 	var $name = 'Organization';
 	var $displayField = 'name';
+	var $validate = array(
+		'name' => array(
+			'name' => array(
+				'rule' => array('notempty'),
+				'message' => 'You must enter a name.',
+//				'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		)
+	);
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
 
 	var $hasOne = array(
--- app/models/subject.php
+++ app/models/subject.php
@@ -5,6 +5,18 @@ class Subject extends AppModel {
 
         var $actsAs = 'ExtendAssociations'; 
 
+	var $validate = array(
+		'name' => array(
+			'name' => array(
+				'rule' => array('notempty'),
+				'message' => 'You must enter a name.',
+//				'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		)
+	);
         //The Associations below have been created with all possible keys, those that are not needed can be removed
 
 	var $belongsTo = array(
@@ -81,9 +93,4 @@ class Subject extends AppModel {
 			'insertQuery' => ''
 		)
 	);
-       /* 
-	function beforeSave($options) {
-		debug($this->data);
-		return false;
-}*/
 }
